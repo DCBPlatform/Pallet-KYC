b7ad26a9913cfc2cb18c09a382ff56a79c993f5e
Afeez Aziz <afeezaziz@gmail.com>
2021-02-07 10:57:14 +0800

    lol

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use frame_support::{
 	decl_event, decl_module, decl_storage,
 	dispatch::DispatchResult,
 };
-use frame_system::{self as system, ensure_signed};
+use frame_system::{self as system, ensure_signed, ensure_root};
 use sp_runtime::RuntimeDebug;
 use sp_std::prelude::*;
 
@@ -18,6 +18,10 @@ pub trait Trait: balances::Trait + system::Trait {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
+type AccountIdOf<T> = <T as system::Trait>::AccountId;
+// type PersonalInformationOf<T> = PairInfo<AccountIdOf<T>, <T as system::Trait>::BlockNumber>;
+
+
 #[derive(Encode, Decode, Clone, Default, RuntimeDebug)]
 pub struct PersonalInformation {
 	name: Vec<u8>,
@@ -25,25 +29,30 @@ pub struct PersonalInformation {
 	email: Vec<u8>,
 	address: Vec<u8>,
 	income: Vec<u8>,
+	nric: Vec<u8>,
+	passport: Vec<u8>,
+	verified: bool
 }
 
 
 decl_storage! {
 	trait Store for Module<T: Trait> as KYCStore {
-		KYCLevel get(fn kyc_level): map hasher(blake2_128_concat) AccountId => u8;
-		Checker get(fn checker): map hasher(blake2_128_concat) AccountId => bool;
+		pub Level get(fn level): map hasher(blake2_128_concat) AccountIdOf<T> => u8;
+		pub Checker get(fn checker): map hasher(blake2_128_concat) AccountIdOf<T> => bool;
+		pub User get(fn user): map hasher(blake2_128_concat) AccountIdOf<T> => Option<PersonalInformation>;
+		pub Users get(fn users): Vec<AccountIdOf<T>>;
 	}
 }
 
 decl_event! (
 	pub enum Event<T>
 	where
-		<T as system::Trait>::AccountId,
-		<T as system::Trait>::Hash,
-		<T as balances::Trait>::Balance
+	<T as system::Trait>::AccountId,
 	{
+		/// Checker added. \[accountId\]
 		CheckerAdded(AccountId),
-		CheckerRemoved(AccountId)
+		/// Checker removed. \[accountId\]
+		CheckerRemoved(AccountId),
 	}
 );
 
@@ -53,45 +62,66 @@ decl_module! {
 
 
 		#[weight = 10_000]
-		fn update_information(origin, kata: Vec<u8>, number: u32, hash: T::Hash, balance: T::Balance) -> DispatchResult {
-			let _ = ensure_signed(origin)?;
-			let thing = InnerThing {
-							kata,
-							number,
-							hash,
-							balance,
-						};
-			<InnerThingsByNumbers<T>>::insert(number, thing);
-			Self::deposit_event(RawEvent::NewInnerThing(number, hash, balance));
+		fn update_information(origin, 
+			name:Vec<u8>, 
+			phone:Vec<u8>, 
+			email:Vec<u8>, 
+			address:Vec<u8>, 
+			income:Vec<u8>, 
+			nric:Vec<u8>, 
+			passport:Vec<u8>) -> DispatchResult {
+			let caller = ensure_signed(origin)?;
+			let salah: bool = false;
+			let thing = PersonalInformation {
+				name: name, 
+				phone: phone, 
+				email: email, 
+				address: address, 
+				income: income, 
+				nric: nric, 
+				passport: passport, 
+				verified: salah
+			};
+			<User<T>>::insert(&caller, thing);
 			Ok(())
+		}		
+
+		#[weight = 10_000]
+		fn validate_information(origin, account: AccountIdOf<T>) -> DispatchResult {
+			let caller = ensure_signed(origin)?;
+			let existing_information = <User<T>>::get(&account);
+			
+			let benar: bool = true;
+			let thing = PersonalInformation {
+				name: existing_information.clone().unwrap().name,
+				phone: existing_information.clone().unwrap().phone,
+				email: existing_information.clone().unwrap().email,
+				address: existing_information.clone().unwrap().address,
+				income: existing_information.clone().unwrap().income,
+				nric: existing_information.clone().unwrap().nric,
+				passport: existing_information.clone().unwrap().passport,
+				verified: benar
+			};
+			<User<T>>::insert(&account, thing);
+			let mut users = <Users<T>>::get();
+			let index = users.len();
+			users.insert(index, account);
+			<Users<T>>::put(users);
+
+			Ok(())
+		}			
+	
+		
+		#[weight = 10_000]
+		fn add_checker(origin, checker: AccountIdOf<T>) {
+			let _ = ensure_root(origin);
+			<Checker<T>>::insert(checker, true)					
 		}	
 		
 		#[weight = 10_000]
-		fn view_information(origin, kata: Vec<u8>, number: u32, hash: T::Hash, balance: T::Balance) -> DispatchResult {
-			let _ = ensure_signed(origin)?;
-			let thing = InnerThing {
-							kata,
-							number,
-							hash,
-							balance,
-						};
-			<InnerThingsByNumbers<T>>::insert(number, thing);
-			Self::deposit_event(RawEvent::NewInnerThing(number, hash, balance));
-			Ok(())
-		}	
-		
-		#[weight = 10_000]
-		fn add_checker(origin, checker: AccountId) -> DispatchResult {
-			let _ = ensure_root(origin)?;
-			<Checker>::put(checker, true)
-			Ok(())
-		}	
-		
-		#[weight = 10_000]
-		fn remove_checker(origin, checker: AccountId) -> DispatchResult {
-			let _ = ensure_root(origin)?;
-			<Checker>::put(checker, false)
-			Ok(())
+		fn remove_checker(origin, checker: AccountIdOf<T>) {
+			let _ = ensure_root(origin);
+			<Checker<T>>::insert(checker, false);					
 		}			
 
 
@@ -100,9 +130,9 @@ decl_module! {
 
 impl<T: Trait> Module<T> {
 
-	pub fn get_information(who: AccountId, what: Vec<u8>) -> Vec<u8> {
-	}
+	// pub fn get_information(who: AccountIdOf<T>, what: Vec<u8>) -> () {
+	// }
 
-	pub fn set_information(who: AccountId, what: Vec<u8>) -> () {
-	}
+	// pub fn set_information(who: AccountIdOf<T>, what: Vec<u8>) -> () {
+	// }
 }
